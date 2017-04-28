1a1eb83c693e5ec8ba54245baa759ea90c71c267
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-29 00:26:17 +0200

    moved in from main folder

---
+++ 2016/install_inputs.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -x
+
+if [ ! -d INPUTS ] ; then 
+    
+    for i in MCC-INPUTS.tgz MCC-INPUTS_MODIFIED.tgz MCC-INPUTS_MODIFIED-2.tgz ;
+    do 
+	if [ ! -f $i ] ; then 
+	    wget http://mcc.lip6.fr/2016/archives/$i
+	fi
+	tar xvzf $i
+	rm -f $i
+    done
+    mv BenchKit/INPUTS .
+    \rm -r BenchKit
+fi
+
