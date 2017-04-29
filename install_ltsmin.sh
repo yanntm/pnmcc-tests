9d6657dbd47401add13003f986847c0b1b17be11
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-04-29 10:03:28 +0200

    use variable consistently

--- install_ltsmin.sh
+++ install_ltsmin.sh
@@ -4,9 +4,9 @@ set -x
 
 if [ ! -d lts_install_dir ] ; then 
     
-    yname=ltsmin_linux.tar.gz
+    yname=ltsmin_linux_64.tar.gz
     if [ ! -f $yname ] ; then 
-	wget "https://yanntm.github.io/ITS-Tools-Dependencies/ltsmin_linux.tar.gz"
+	wget "https://yanntm.github.io/ITS-Tools-Dependencies/$yname"
     fi
 
     tar xzf $yname
