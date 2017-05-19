99857dfd4f059cfd151ca7a4af222b72d36906fd
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-05-19 20:04:48 +0200

    new installer for single input

---
+++ install_input.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+set -x
+
+if [ ! -d INPUTS ] ; then
+    mkdir INPUTS
+fi
+
+cd INPUTS
+
+if [ ! -f $1.tgz ] ; then 
+    wget https://yanntm.github.io/pnmcc-models-2017/$1.tgz
+fi
+
+tar xzf $1.tgz
+
+cd ..
+
