f9197c1b02495b973284f14ba1f2d8e432f9effd
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-06-13 09:25:21 +0200

    not the way to use travis_retry

--- install_input.sh
+++ install_input.sh
@@ -9,7 +9,7 @@ fi
 cd INPUTS
 
 if [ ! -f $1.tgz ] ; then 
-    travis_retry wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2017/$1.tgz
+    wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2017/$1.tgz
 fi
 
 tar xzf $1.tgz
