d5ebd508e1cd7298093ec9f62bf054bd106f23c2
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-12-01 20:39:03 +0100

    updated to work with the google sheets built from contest data

--- csv_to_control2.pl
+++ csv_to_control2.pl
@@ -4,26 +4,31 @@
 my @index = ("0","1","10","11","12","13","14","15","2","3","4","5","6","7","8","9");
 
 while (my $line = <STDIN>) {
- # print $line;
+    # print $line;
+    chomp $line;
   my @fields = split /,/, $line;
-  my $prefix = @fields[0]."-".@fields[1]."-";
+  my $prefix = @fields[0]."-".@fields[1];
   my @verdicts = split //, @fields[2];
 
-  if ($#verdicts < 16) {
+  if ($#verdicts != 16 && $#verdicts != 1) {
     next;
   }
   my $abbrev = @fields[1];
   $abbrev =~ s/[a-z]//g;
   my $outff = @fields[0]."-".$abbrev.".out";
 
-  print "doing $prefix, in file $outff has ".($#verdicts)." entries \n";  
-  open OUT, "> $outff";
-  print OUT "./runatest.sh ".@fields[0]." ".@fields[1] ."\n";
-  for (my $i=0 ; $i < $#verdicts ; $i++) {
-    my $res = @verdicts[$i];   
-    $res =~ s/F/FALSE/g;
-    $res =~ s/T/TRUE/g;
-    print OUT "FORMULA ".$prefix.@index[$i]." ".$res." TECHNIQUES ORACLE2015\n";
+  if (-f $outff) {
+      print "Not overwriting existing oracle file $outff\n";
+  } else {
+      print "doing $prefix, in file $outff has ".($#verdicts)." entries \n";  
+      open OUT, "> $outff";
+      print OUT "./runatest.sh ".@fields[0]." ".@fields[1] ."\n";
+      for (my $i=0 ; $i < $#verdicts ; $i++) {
+	  my $res = @verdicts[$i];   
+	  $res =~ s/F/FALSE/g;
+	  $res =~ s/T/TRUE/g;
+	  print OUT "FORMULA ".$prefix."-".@index[$i]." ".$res." TECHNIQUES ORACLE2017\n";
+      }
+      close OUT;
   }
-  close OUT;
 }
