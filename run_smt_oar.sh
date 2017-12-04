e8c29c5e8d6ec4a48fad45fcd9e5b841e0316fd7
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-12-04 19:20:15 +0100

    enforce nodes=1, add big14

--- run_smt_oar.sh
+++ run_smt_oar.sh
@@ -4,6 +4,6 @@ set -e
 
 for i in oracle/*RC.out oracle/*RF.out ;
 do
-      oarsub -l "{host='big6' or host='big7' or host='big8' or host='big9' or host='big10' or host='big11' or host='big12' or host='big13'}/core=2"  --notify "mail:yann.thierry-mieg@lip6.fr" "cd /home/ythierry/test/ITS-Tools-pnmcc && ./run_test.pl $i -smt"
+      oarsub -l "{host='big6' or host='big7' or host='big8' or host='big9' or host='big10' or host='big11' or host='big12' or host='big13' or host='big14'}/nodes=1/core=2"  --notify "mail:yann.thierry-mieg@lip6.fr" "cd /home/ythierry/test/ITS-Tools-pnmcc && ./run_test.pl $i -smt"
 done;
 
