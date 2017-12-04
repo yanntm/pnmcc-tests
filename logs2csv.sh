ec942551c5892fd77c66bbfcefbcf906828ea533
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-12-04 19:51:08 +0100

    improved script : identify run from log

--- logs2csv.sh
+++ logs2csv.sh
@@ -2,11 +2,14 @@
 
 for log in *out ;
 do
-    model=$(echo $log | sed 's/.out//g');
+    line=$(cat $log | grep syscalling)
+    model=$(echo $line | cut -f 4 -d ' ')
+    exam=$(echo $line | cut -f 5 -d ' ')
+    tech=$(echo $line | cut -f 6- -d ' ' | sed -e 's#/[^ ]* ##g')
     tot=$(cat $log | grep testStarted | wc -l)
     fail=$(cat $log | grep testFailed | wc -l)
     fin=$(cat $log | grep testFinished | wc -l)
     dur=$(cat $log | grep testFinished | grep -e '\ball\b' | sed  's/[^0-9]//g')
-    echo "$model,$tot,$fail,$fin,$dur"
+    echo "$model,$exam,$tech,$tot,$fail,$fin,$dur"
 done
 
