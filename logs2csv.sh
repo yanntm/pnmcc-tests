6419731817db55cc2ba977777945f90c9b24c7eb
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-12-04 15:37:54 +0100

    protect against string "all" in model name

--- logs2csv.sh
+++ logs2csv.sh
@@ -6,7 +6,7 @@ do
     tot=$(cat $log | grep testStarted | wc -l)
     fail=$(cat $log | grep testFailed | wc -l)
     fin=$(cat $log | grep testFinished | wc -l)
-    dur=$(cat $log | grep testFinished | grep all | sed  's/[^0-9]//g')
+    dur=$(cat $log | grep testFinished | grep -e '\ball\b' | sed  's/[^0-9]//g')
     echo "$model,$tot,$fail,$fin,$dur"
 done
 
