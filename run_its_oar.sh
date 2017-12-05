3840f7065556c2d1c2bf6c8e20f281e332975338
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2017-12-05 17:52:58 +0100

    force exit at end of run

---
+++ run_its_oar.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+set -e
+
+for i in oracle/*.out ;
+do
+      oarsub -l "{host='big6' or host='big7' or host='big8' or host='big9' or host='big10' or host='big11' or host='big12' or host='big13' or host='big14'}/nodes=1/core=1,walltime=0:12:0"  --notify "mail:yann.thierry-mieg@lip6.fr" "cd /home/ythierry/test/ITS-Tools-pnmcc && ./run_test.pl $i -its ; exit" 
+done;
+
