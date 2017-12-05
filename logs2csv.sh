4a500caa26f05a50411caed5e61f5c9fb6e617e8
Yann THIERRY-MIEG <ythierry@poleia.lip6.fr>
2017-12-05 17:47:38 +0100

    more columns

--- logs2csv.sh
+++ logs2csv.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+
+echo "log,Model,Examination,Techniques,Test started,Test fail,Test fin,duration(ms),Initial,Tautology,ITS,BMC,Induction,PINS,PINSPOR"
 for log in *out ;
 do
     line=$(cat $log | grep syscalling)
@@ -10,6 +12,13 @@ do
     fail=$(cat $log | grep testFailed | wc -l)
     fin=$(cat $log | grep testFinished | wc -l)
     dur=$(cat $log | grep testFinished | grep -e '\ball\b' | sed  's/[^0-9]//g')
-    echo "$model,$exam,$tech,$tot,$fail,$fin,$dur"
+    init=$(cat $log | grep TECHNIQUES | grep INITIAL_STATE | wc -l)
+    taut=$(cat $log | grep TECHNIQUES | grep TAUTOLOGY | wc -l)
+    sdd=$(cat $log | grep TECHNIQUES | grep DECISION_DIAGRAMS | wc -l)
+    bmc=$(cat $log | grep TECHNIQUES | grep BMC | wc -l)
+    kind=$(cat $log | grep TECHNIQUES | grep K_INDUCTION | wc -l)
+    pins=$(cat $log | grep TECHNIQUES | grep LTSMIN | grep -v PARTIAL_ORDER | wc -l)
+    por=$(cat $log | grep TECHNIQUES | grep LTSMIN | grep PARTIAL_ORDER | wc -l)
+    echo "$log,$model,$exam,$tech,$tot,$fail,$fin,$dur,$init,$taut,$sdd,$bmc,$kind,$pins,$por"
 done
 
