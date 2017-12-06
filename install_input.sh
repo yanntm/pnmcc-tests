d07e3e716880d24aa83186bcf636c6eb97d6df01
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2017-12-06 22:05:00 +0100

    use a new folder for each run
    sharing folders is an issue on big

--- install_input.sh
+++ install_input.sh
@@ -12,8 +12,13 @@ if [ ! -f $1.tgz ] ; then
     wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2017/$1.tgz
 fi
 
-if [ ! -d $1 ] ; then
-    tar xzf $1.tgz
+if [ ! -d $2 ] ; then
+    mkdir $2
+    cd $2
+    tar xzf ../$1.tgz
+    mv $1/* .
+    \rm -r $1
+    cd ..
 fi
 
 cd ..
