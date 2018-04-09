cecb43f4a81b80971d6aed2862dfeab3d9888b4f
Yann Thierry-Mieg <yann.thierry-mieg@lip6.fr>
2018-04-09 22:54:44 +0200

    Going for the RCP build now

--- install_eclipse.sh
+++ install_eclipse.sh
@@ -2,10 +2,13 @@
 
 set -x
 
-if [ ! -f eclipse/eclipse ] ; then 
-	wget --progress=dot:mega https://yanntm.github.io/ITS-commandline/itscl_linux.tgz
-	tar xzf itscl_linux.tgz
-	rm itscl_linux.tgz
+if [ ! -f itstools/its-tools ] ; then 
+	mkdir itstools
+	cd itstools
+	wget --progress=dot:mega https://yanntm.github.io/ITS-commandline/fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip
+	unzip fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip
+	rm fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip
+	cd ..
 fi
 
 
