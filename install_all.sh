51e145d68ffc00dc0600ede502ed43f1d9a54926
ythierry <ythierry@localhost>
2019-05-18 17:47:06 +0200

    grab 2019 models, formulas and oracles

--- install_all.sh
+++ install_all.sh
@@ -13,7 +13,7 @@ set -e
 ./install_z3.sh
 
 # ./install_inputs.sh
-wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2018/oracle.tar.gz
+wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2019/oracle.tar.gz
 tar xzf oracle.tar.gz
 
 # yices page down Jun 18 : don't break builds we use z3
--- install_input.sh
+++ install_input.sh
@@ -9,7 +9,7 @@ fi
 cd INPUTS
 
 if [ ! -f $1.tgz ] ; then 
-    wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2018/INPUTS/$1.tgz
+    wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2019/INPUTS/$1.tgz
 fi
 
 if [ ! -d "$1$2" ] ; then
@@ -17,7 +17,6 @@ if [ ! -d "$1$2" ] ; then
     cd "$1$2"
     tar xzf ../$1.tgz
     mv $1/* .
-    cat ../../GlobalProperties.xml | sed "s/MODELNAME/$1/g" > GlobalProperties.xml     
     \rm -r $1
     cd ..
 fi
