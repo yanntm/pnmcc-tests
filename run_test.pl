ad6323e67671cb150ba71bb0fba04ec254be0c0d
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-02-18 18:02:25 +0100

    remove exception on TRANSITIONS count in state space category

--- run_test.pl
+++ run_test.pl
@@ -140,10 +140,10 @@ $o = keys (%formouts);
 $e = keys (%verdicts);
 print "\n##teamcity[testStarted name='all']\n";
 if ( $o != $e ) {
-  unless ( $title =~ /SS/ && $o == 3 && $e == 4) {
+#  unless ( $title =~ /SS/ && $o == 3 && $e == 4) {
  	 print "\n##teamcity[testFailed name='all' message='regression detected : less results than expected ( $o / $e )' details='' expected='$e' actual='$o'] \n";
   	$failed++;
-  }
+#  }
 } elsif ($o > 0) {
   print "All $o tests successful in suite : $title\n";
 }
