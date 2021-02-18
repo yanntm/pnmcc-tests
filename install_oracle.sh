3c1f853bb0fb193d5571b5944abc757a1fb33f07
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-02-18 18:24:39 +0100

    if then else ?

--- install_oracle.sh
+++ install_oracle.sh
@@ -3,10 +3,11 @@
 set -x
 set -e
 
-if [ ! -f oracle.tar.gz ] 
-		wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2020/oracle.tar.gz
+if [ ! -f oracle.tar.gz ] ; then 
+	wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2020/oracle.tar.gz 
 fi
-if [ ! -d oracle/ ] 
-		tar xzf oracle.tar.gz
+
+if [ ! -d oracle/ ] ; then 
+	tar xzf oracle.tar.gz 
 fi
 
