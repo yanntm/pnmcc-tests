646fad76274e62a24c43bde78fb1afc1bd9a492c
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-02-18 18:21:23 +0100

    bash syntax error

--- install_oracle.sh
+++ install_oracle.sh
@@ -3,12 +3,12 @@
 set -x
 set -e
 
-if [ ! -f oracle.tar.gz ] ;
+if [ ! -f oracle.tar.gz ] 
 	do
 		wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2020/oracle.tar.gz
 	done
 fi
-if [ ! -d oracle/ ] ;
+if [ ! -d oracle/ ] 
 	do 
 		tar xzf oracle.tar.gz
 	done
