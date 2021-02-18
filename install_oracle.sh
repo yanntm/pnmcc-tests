80f64a3c8092fad7566340d0b5351d9929770da4
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2021-02-18 18:22:57 +0100

    more syntax errors

--- install_oracle.sh
+++ install_oracle.sh
@@ -4,13 +4,9 @@ set -x
 set -e
 
 if [ ! -f oracle.tar.gz ] 
-	do
 		wget --progress=dot:mega https://yanntm.github.io/pnmcc-models-2020/oracle.tar.gz
-	done
 fi
 if [ ! -d oracle/ ] 
-	do 
 		tar xzf oracle.tar.gz
-	done
 fi
 
